e0abf6624ba81c57fdfa181f968978c729198553
Younes Lahouiti <loller763@gmail.com>
2023-05-16 14:34:52 +0200

    Debugged the cursor effect

--- src/utils/classes/particles/pixel-particle.class.ts
+++ src/utils/classes/particles/pixel-particle.class.ts
@@ -147,8 +147,11 @@ export class PixelParticle {
    * @returns {void}
    */
   update(mouseX: number, mouseY: number, mouseRadius = 20_000): void {
-    this.mouseParticleDistanceX = mouseX - this.x;
-    this.mouseParticleDistanceY = mouseY - this.y;
+    //We get the top left coordinates of our canvas
+    const { x, y }: DOMRect = this.context.canvas.getBoundingClientRect();
+
+    this.mouseParticleDistanceX = mouseX - this.x - x;
+    this.mouseParticleDistanceY = mouseY - this.y - y;
 
     //We're not going to compute the hypothenuse with `Math.sqrt()` for performance reasons
     this.mouseTotalDistance =
